00b77881ff795d475ebc463b000ad71c4c82688a
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 18:01:40 -0400

    Updated header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-dash" role="navigation">
+    <nav className="navbar navbar-dash">
       <div className="container-fluid">
         <div className="navbar-header">
           <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
@@ -12,19 +12,16 @@ const Header = () => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="#">React Dashboard</a>
+          <a className="navbar-brand" href="/">React Dashboard</a>
         </div>
 
         <div className="collapse navbar-collapse" id="navbar">
           <ul className="nav navbar-nav">
-            <li className="active"><a href="#">Home</a></li>
-            <li><a href="#"></a></li>
+            <li className="active"><a href="/">Home</a></li>
 
           </ul>
-
           <ul className="nav navbar-nav navbar-right">
-            <li><a href="#"></a></li>
-
+            <li><a href="/login">Login</a></li>
           </ul>
         </div>
       </div>
