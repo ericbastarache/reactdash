62f240d0bd75036d694487de109eed00d6ba1bc1
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 18:01:53 -0400

    Updated main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Card from '../../components/Card/Card';
 
-const Main = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <h1 className="text-center">React Dashboard</h1>
+class Main extends Component{
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">React Dashboard</h1>
+          </div>
+        </div>
+        <div className="row">
+          <Card cardHeader="Simple"/>
+          <Card cardHeader="Fast"/>
+          <Card cardHeader="Reliable"/>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Main;
