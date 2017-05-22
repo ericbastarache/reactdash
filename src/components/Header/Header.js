4240ca26c9ec75c0674cb2cc6f0186f7585c6620
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 16:56:01 -0400

    Updated header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-default" role="navigation">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#"></a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Home</a></li>
+            <li><a href="#"></a></li>
+
+          </ul>
+          
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#"></a></li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
