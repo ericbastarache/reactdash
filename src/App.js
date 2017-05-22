10642534d0923fd17167fb5bfe16affcac078444
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 17:26:10 -0400

    Added main component to entry file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+          <Main />
         <Footer />
       </div>
     );
