faddf8dd4e4a88428e5066ca820ae6049b21a8dd
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 15:12:35 -0400

    Created footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <p className="text-center">Copyright Eric Bastarache 2017</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
