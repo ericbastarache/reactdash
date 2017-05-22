73b809950b64c11acee0fcefd33827deb556597c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-22 17:26:56 -0400

    Created initial main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">React Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
