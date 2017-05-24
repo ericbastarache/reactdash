3b04c26040220f2929583ca008e7f6797bc60b37
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-24 16:38:42 -0400

    Created initial login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+  render () {
+    return (
+      <h1>Hello Login</h1>
+    );
+  }
+}
+
+export default Login;
