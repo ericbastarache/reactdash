82df032271226abd12362d200f8762c9566c016c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-24 04:25:58 -0400

    Updated main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import './Main.css';
 import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
 
 class Main extends Component{
   render () {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1 className="text-center">React Dashboard</h1>
+      <div>
+        <section className="text-holder">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="text-center">React Dashboard</h1>
+              </div>
+            </div>
+            <div className="row">
+              <Card cardHeader="Simple" cardDescription="React Dash gives you valuable insights into who's doing what on your website."/>
+              <Card cardHeader="Fast" cardDescription="With no page reloads, you can get realtime information as fast as you need it"/>
+              <Card cardHeader="Reliable" cardDescription="React Dash is operational as long as you have an internet connection"/>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <Card cardHeader="Simple" cardDescription="asd"/>
-          <Card cardHeader="Fast" cardDescription="fgh"/>
-          <Card cardHeader="Reliable" cardDescription="jkl"/>
-        </div>
-      </div>
+        </section>
+        <section className="button-holder">
+          <div className="container">
+            <div className="row">
+              <Button buttonClassName="interesting" buttonText="Click Me!"/>
+              <Button buttonClassName="show-content" buttonText="Interesting Content..." />
+            </div>
+          </div>
+        </section>
+    </div>
     );
   }
 }
