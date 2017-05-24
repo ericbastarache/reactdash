1c17d120fdb53d2382f7a6dbdbc09fd30003d133
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-24 04:25:40 -0400

    Updated card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({cardHeader, cardDescription}) => {
+const Card = (props) => {
   return (
     <div className="col-md-4">
       <div className="card">
-        <h1 className="text-center">{cardHeader}</h1>
-        <p>{cardDescription}</p>
+        <h1 className="text-center">{props.cardHeader}</h1>
+        <p>{props.cardDescription}</p>
       </div>
     </div>
   )
