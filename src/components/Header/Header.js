f0b7c802bbad7e5aab2d7cb31cd4217aa4309f1c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-24 16:38:33 -0400

    Updated header component to use link from react router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -12,16 +13,16 @@ const Header = () => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="/">React Dashboard</a>
+          <Link className="navbar-brand" to="/">React Dashboard</Link>
         </div>
 
         <div className="collapse navbar-collapse" id="navbar">
           <ul className="nav navbar-nav">
-            <li className="active"><a href="/">Home</a></li>
-
+            <li className="active"><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
           </ul>
           <ul className="nav navbar-nav navbar-right">
-            <li><a href="/login">Login</a></li>
+            <li><Link to="/login">Login</Link></li>
           </ul>
         </div>
       </div>
