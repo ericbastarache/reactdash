36ff33293e215ed0667dc6c9f23dc8430f2516dd
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-24 17:22:29 -0400

    Updated login component and added two initial methods for form handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,39 @@ import React, { Component } from 'react';
 import './Login.css';
 
 class Login extends Component {
+
+  handleChange () {
+
+  }
+
+  handleSubmit () {
+
+  }
+
   render () {
     return (
-      <h1>Hello Login</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-4">
+            <div className="login-holder">
+              <h1 className="text-center">Login</h1>
+              <form onSubmit={this.handleSubmit} className="form-horizontal">
+                <div className="form-group">
+                  <label htmlFor="username">Username: </label>
+                  <input name="username" className="form-control" type="text" onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password: </label>
+                  <input name="password" type="password" className="form-control" onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <button className="btn btn-primary">Sign In</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
