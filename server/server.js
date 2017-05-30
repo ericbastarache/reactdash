9ff4f8a00bf34923d9155a7f5aa39fbd0d5a7606
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-30 18:11:52 -0400

    Created initial server file

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const ws = require('ws');
+const geoip = require('geoip-lite');
+const useragent = require('useragent');
+
+const app = express();
+const server = http.Server(app);
+const wss = new ws.Server({ server: server, path: '/', clientTracking: false, maxPayload: 1024 });
+
+let config = {
+  port: 8080,
+  wshost: 'ws://localhost:8080'
+}
+
+app.disable('x-powered-by');
+server.listen(config.port);
